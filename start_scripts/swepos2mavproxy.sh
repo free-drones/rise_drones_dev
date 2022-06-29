819097ae054d91ee2cb6b91c5a189849d24e29eb
Andreas Gising <a.gising@gmail.com>
2022-06-29 10:53:30 +0200

    N-RTK settings from late test flight

--- start_scripts/swepos2mavproxy.sh
+++ start_scripts/swepos2mavproxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/pi/.hx_config
-/home/pi/.venv/rise_drones/bin/python3 /home/pi/rise_drones/src/app/ntripclient-py/ntrip_client.py --user $NTRIP_USER --password $NTRIP_PWD --server $NTRIP_SERVER --mountpoint $NTRIP_MOUNTPOINT --description "$NTRIP_MNTP_DESCR" --verbose
+/home/pi/.venv/rise_drones/bin/python3 /home/pi/rise_drones/src/ntripclient-py/ntrip_client.py --user $NTRIP_USER --password $NTRIP_PWD --server $NTRIP_SERVER --mountpoint $NTRIP_MOUNTPOINT --description "$NTRIP_MNTP_DESCR" --port $NTRIP_PORT --verbose
 echo "STREAMING RTCM DATA FROM SWEPOS"
 exit
