cbd206aa62c6ebf853a3945ced8acf39dfa42a0e
Kristoffer Bergman <kristoffer.bergman@ri.se>
2022-07-06 09:19:26 +0200

    Added script for starting app geopoint

---
+++ start_scripts/app_geopoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Run application app geopoint
+# Load the local variables for HX_IP
+source /home/pi/.hx_config
+cd /home/pi/rise_drones/src/app
+/home/pi/.venv/rise_drones/bin/python3 app_geopoint --crm=10.44.160.10:16300 --app_ip=$HX_IP --stdout --log=info
