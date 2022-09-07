d260fe577dadd602600b48bdc5b4bad99768447d
hx-003 <rise.hx003@gmail.com>
2022-09-07 19:44:57 +0000

    copy paste fix

--- start_scripts/commander/app_monitor.sh
+++ start_scripts/commander/app_monitor.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-app=../../../rise_drones/src/app/app_ussp_mission.py
-mission=../../mission/missions/USSP_Granso_LMD.json
+app=../../../rise_drones/src/app/app_monitor.py
 echo "app: $app"
-echo "mission: $mission"
-/home/droneadmin/.venv/rise_drones/bin/python3 $app --crm=10.44.160.10:16300 --app_ip=10.44.160.10 --mission=$mission --stdout --log=info --capabilities C0
+/home/droneadmin/.venv/rise_drones/bin/python3 $app --crm=10.44.160.10:16300 --app_ip=10.44.160.10 --stdout --log=info --capabilities None
