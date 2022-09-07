1ad402aaac89974161991f54000f740dd6fcdbed
Andreas Gising <a.gising@gmail.com>
2022-09-07 16:52:23 +0200

    paths

--- start_scripts/commander/USSP_dji_mini_granso.sh
+++ start_scripts/commander/USSP_dji_mini_granso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-app=app_lmd_granso.py
-mission=../../../rise_drones_dev/mission/missions/USSP_Granso_LMD.json
+app=../../../rise_drones/src/app/app_lmd_granso.py
+mission=../../mission/missions/USSP_Granso_LMD.json
 echo "app: $app"
 echo "mission: $mission"
-cd /home/dronehost/rise_drones/app
+cd /home/droneadmin/tyra/rise_drones/app
 /home/droneadmin/.venv/rise_drones/bin/python3 $app --crm=10.44.160.10:16300 --app_ip=10.44.160.10 --mission=$mission --stdout --log=info
