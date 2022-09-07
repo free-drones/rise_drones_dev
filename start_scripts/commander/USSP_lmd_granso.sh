b1863f72f998bde044bb17c2e20289d23effbcc8
hx-003 <rise.hx003@gmail.com>
2022-09-07 19:44:57 +0000

    copy paste fix

--- start_scripts/commander/USSP_lmd_granso.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-app=../../../rise_drones/src/app/app_monitor.py
-echo "app: $app"
-/home/droneadmin/.venv/rise_drones/bin/python3 $app --crm=10.44.160.10:16300 --app_ip=10.44.160.10 --stdout --log=info --capabilities None
