66633c5eff1844546931cf68bc873d547f6765dc
Kristoffer Bergman <kristoffer.bergman@ri.se>
2022-09-13 15:53:18 +0200

    New start scripts for demo

---
+++ start_scripts/app_ussp_camera_saab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Load the local variables for HX_IP
+source /home/pi/.hx_config
+cd /home/pi/rise_drones/src/app
+/home/pi/.venv/rise_drones/bin/python3 app_ussp_mission.py --crm=10.44.160.10:16300 --app_ip=$HX_IP --mission=../../../rise_drones_dev/mission/missions/USSP_SAAB_CAMERA1.json --stdout --log=info --capabilities $HX_CAPABILITIES
