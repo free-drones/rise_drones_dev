4c021764fe7369c9911de0fdd9c5d78b4a011352
Kristoffer Bergman <kristoffer.bergman@ri.se>
2022-09-13 15:58:12 +0200

    streaming to fix IP

--- video_streaming/stream_to_waraps.sh
+++ video_streaming/stream_to_waraps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Process will live until abort is sent from script server
 
-ffmpeg -f v4l2 -i /dev/video0 -c libx264 -pix_fmt yuv420p -preset ultrafast -f flv -b:v 0 -g 1 -tune zerolatency -crf 25 rtmp://ome.waraps.org/app/RISE-HX-003
+ffmpeg -f v4l2 -i /dev/video0 -c libx264 -pix_fmt yuv420p -preset ultrafast -f flv -b:v 0 -g 1 -tune zerolatency -crf 25 rtmp://10.10.10.21/app/RISE-HX-003
