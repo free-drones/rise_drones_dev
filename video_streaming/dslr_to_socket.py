e6f801c13768073727ff3e2b441d0829e717c1fd
Kristoffer Bergman <kristoffer.bergman@ri.se>
2022-09-26 08:53:27 +0200

    Remove unused code

--- video_streaming/dslr_to_socket.py
+++ video_streaming/dslr_to_socket.py
@@ -6,11 +6,10 @@ from datetime import datetime
 
 HOST = "192.168.2.3"
 PORT = 13330
-host = socket.gethostbyname(HOST)
 send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print(f"trying to connect to {host}:{PORT}")
+print(f"trying to connect to {HOST}:{PORT}")
 send_sock.connect((HOST, PORT))
-print(f"Connected to {host}:{PORT}")
+print(f"Connected to {HOST}:{PORT}")
 
 camera = gp.Camera()
 camera.init()
@@ -20,19 +19,6 @@ while True:
    capture = camera.capture_preview()
    filedata = capture.get_data_and_size()
    data = memoryview(filedata)
-   #print(f"data size: {len(data.tobytes())}")
    send_sock.sendall(data.tobytes())
    end_t = datetime.timestamp(datetime.now())
    time.sleep(max(1/fps - (end_t-start_t), 0))
-
-
-#while True:
-#
-#   start_t = datetime.timestamp(datetime.now())
-#   capture = camera.capture_preview()
-#   filedata = capture.get_data_and_size()
-#   data = memoryview(filedata)
-#   ffmpeg.stdin.write(data.tobytes())
-#   end_t = datetime.timestamp(datetime.now())
-#   time.sleep(max(1/fps - (end_t-start_t), 0)
-
